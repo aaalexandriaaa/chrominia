913ebe3aeb157ae37d65c1f0bb4c0f1ab91ead42
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-18 14:36:57 -0400

    update 3 hrefs to correct

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,12 @@ const NavBar = ({ user, handleLogout }) => {
     <>
       {user ?
         <nav>
-          <div><a href="/profilepage" >Welcome, {user.name}</a></div>
+          <div><a href="/profile" >Welcome, {user.name}</a></div>
           <div><a href="/">Home</a></div>
           <div><a href="/projects">Projects</a></div>
           <div><a href="/gallery">Gallery</a></div>
-          <div><a href="/supplies">Supplies</a></div>
-          <div><a href="/wishlist">Wish List</a></div>
+          <div><a href="/allsupplies">Supplies</a></div>
+          <div><a href="/viewwishlist">Wish List</a></div>
           <div><a href=" " onClick={handleLogout}>Log Out</a></div>
         </nav>
         :
