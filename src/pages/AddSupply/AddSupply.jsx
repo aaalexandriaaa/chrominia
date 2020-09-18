7149362a52e499b78d64c4c72a256e27540b1d15
Jam Wheat <jamonwheat@gmail.com>
2020-09-18 14:02:43 -0400

    stub supplie views

---
+++ src/pages/AddSupply/AddSupply.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class AddSupply extends Component {
+  state = {
+
+  }
+
+  render() { 
+    return (
+      <>
+        <h1>Add Supply</h1>
+      </>
+    );
+  }
+}
+ 
+export default AddSupply;
---
+++ src/pages/ViewModels/ViewModels.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class ViewModels extends Component {
+  state = {
+
+  }
+
+  render() { 
+    return (
+      <>
+        <h1>View Models</h1>
+      </>
+    );
+  }
+}
+ 
+export default ViewModels;
