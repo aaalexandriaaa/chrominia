9440d2130837292944353395dcb7a29e679bc1cb
cem aytac <cenko@MacBook-Air-3.local>
2020-09-18 14:51:23 -0400

    add image routes and controller

---
+++ routes/images.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const imagesCtrl = require('../controllers/images')
+
+// Public Routes
+router.get('/', imagesCtrl.index);
+
+
+// Protected Routes
+router.use(require('../config/auth'));
+// router.post('/', checkAuth, imagesCtrl.create);
+// router.delete('/:id', checkAuth, imagesCtrl.delete);
+// router.put('/:id', checkAuth, imagesCtrl.update);
+
+function checkAuth(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({ msg: 'Not Authorized' });
+}
+
+module.exports = router;
