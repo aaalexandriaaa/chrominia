0180c4df3bb2695485a7561e820fb9774367dfba
Jam Wheat <jamonwheat@gmail.com>
2020-09-18 14:30:49 -0400

    add routes to app.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,6 +5,27 @@ import Signup from "../Signup/Signup";
 import Login from "../Login/Login";
 import authService from "../../services/authService";
 import "./App.css";
+import AddImage from '../AddImage/AddImage'
+import AddProject from '../AddProject/AddProject'
+import AddSupply from '../AddSupply/AddSupply'
+import AllSupplies from '../AllSupplies/AllSupplies'
+import EditImage from '../EditImage/EditImage'
+import EditProfile from '../EditProfile/EditProfile'
+import EditProject from '../EditProject/EditProject'
+import Gallery from '../Gallery/Gallery'
+import LandingPage from '../LandingPage/LandingPage'
+import ProfilePage from '../ProfilePage/ProfilePage'
+import ProjectDetails from '../ProjectDetails/ProjectDetails'
+import Projects from '../Projects/Projects'
+import ViewBrushes from '../ViewBrushes/ViewBrushes'
+import ViewImage from '../ViewImage/ViewImage'
+import ViewMaterials from '../ViewMaterials/ViewMaterials'
+import ViewModels from '../ViewModels/ViewModels'
+import ViewOther from '../ViewOther/ViewOther'
+import ViewPaintAccs from '../ViewPaintAccs/ViewPaintAccs'
+import ViewPaints from '../ViewPaints/ViewPaints'
+import ViewTools from '../ViewTools/ViewTools'
+import ViewWishList from '../ViewWishList/ViewWishList'
 
 class App extends Component {
   state = {
@@ -29,9 +50,7 @@ class App extends Component {
           exact
           path="/"
           render={() => (
-            <main>
-              <h1>Welcome to Chrominia</h1>
-            </main>
+            <LandingPage />
           )}
         />
         <Route
@@ -53,6 +72,146 @@ class App extends Component {
               handleSignupOrLogin={this.handleSignupOrLogin}
             />
           )}
+        />
+        <Route
+        exact
+        path="/addimage"
+        render={() => (
+          <AddImage />
+        )}
+        />
+        <Route
+        exact
+        path="/addproject"
+        render={() => (
+          <AddProject />
+        )}
+        />
+        <Route
+        exact
+        path="/addsupply"
+        render={() => (
+          <AddSupply />
+        )}
+        />
+        <Route
+        exact
+        path="/allsupplies"
+        render={() => (
+          <AllSupplies />
+        )}
+        />
+        <Route
+        exact
+        path="/editimage"
+        render={() => (
+          <EditImage />
+        )}
+        />
+        <Route
+        exact
+        path="/editprofile"
+        render={() => (
+          <EditProfile />
+        )}
+        />
+        <Route
+        exact
+        path="/editproject"
+        render={() => (
+          <EditProject />
+        )}
+        />
+        <Route
+        exact
+        path="/gallery"
+        render={() => (
+          <Gallery />
+        )}
+        />
+        <Route
+        exact
+        path="/profile"
+        render={() => (
+          <ProfilePage />
+        )}
+        />
+        <Route
+        exact
+        path="/projectdetails"
+        render={() => (
+          <ProjectDetails />
+        )}
+        />
+        <Route
+        exact
+        path="/projects"
+        render={() => (
+          <Projects />
+        )}
+        />
+        <Route
+        exact
+        path="/viewbrushes"
+        render={() => (
+          <ViewBrushes />
+        )}
+        />
+        <Route
+        exact
+        path="/wiewimage"
+        render={() => (
+          <ViewImage />
+        )}
+        />
+        <Route
+        exact
+        path="/viewmaterials"
+        render={() => (
+          <ViewMaterials />
+        )}
+        />
+        <Route
+        exact
+        path="/viewmodels"
+        render={() => (
+          <ViewModels />
+        )}
+        />
+        <Route
+        exact
+        path="/viewother"
+        render={() => (
+          <ViewOther />
+        )}
+        />
+        <Route
+        exact
+        path="/viewpaintaccs"
+        render={() => (
+          <ViewPaintAccs />
+        )}
+        />
+        <Route
+        exact
+        path="/viewpaints"
+        render={() => (
+          <ViewPaints />
+        )}
+        />
+        <Route
+        exact
+        path="/viewtools"
+        render={() => (
+          <ViewTools />
+        )}
+        />
+        <Route
+        exact
+        path="/viewwishlist"
+        render={() => (
+          <ViewWishList />
+        )}
         />
       </>
     );
