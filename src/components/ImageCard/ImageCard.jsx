93dad597ccaec19dfa74320eab41b2d07a265bda
Jam Wheat <jamonwheat@gmail.com>
2020-09-20 09:44:21 -0400

    ammend img tag in ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -4,7 +4,7 @@ const ImageCard = ( {image} ) => {
   return (
     <div>
       <h3>{image.title}</h3>
-      <img src={image.url} width="400"></img>
+      <img src={image.url} width="400" alt={image.description}></img>
       <p>{image.description}</p>
     </div>
   );
