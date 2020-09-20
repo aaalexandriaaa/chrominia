929c0008fddabe70a0b98a65097e95bcbe30beb6
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-19 20:56:09 -0400

    very basic form for add project and toher functions

--- src/pages/AddProject/AddProject.jsx
+++ src/pages/AddProject/AddProject.jsx
@@ -1,15 +1,56 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import * as projectAPI from '../../services/projects-api'
 
 class AddProject extends Component {
-    state = {}
+    state = {
+        invalidForm: false,
+        formData: {
+            name: ''
+        }
+    }
+
+    formRef = React.createRef()
+
+    handleAddProject = async newProjectData => {
+        console.log("ADDPROJECT")
+        await projectAPI.create(newProjectData)
+        .then(() => this.props.history.push('/projects'))
+        console.log("PROJECTADDED")
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.handleAddProject(this.state.formData)
+    }
+
+    handleChange = e => {
+        const formData = { ...this.state.formData, [e.target.name]: e.target.value };
+        this.setState({
+            formData,
+            invalidForm: !this.formRef.current.checkValidity()
+        });
+    }
+
     render() {
         return (
             <>
-                <h1>Add Project</h1>
                 <Link to="/projects">
                     Projects
                 </Link><br />
+                <h1>Add Project</h1>
+                <form className="col s12" ref={this.formRef} onSubmit={this.handleSubmit}>
+                    <div>
+                        <label >name</label><br></br>
+                        <input name="name" type="text" value={this.state.formData.name} onChange={this.handleChange} required />
+                    </div>
+                    <button
+                        type="submit"
+                        disabled={this.state.invalidForm}
+                    >
+                        Add Project
+                    </button>
+                </form>
             </>
         );
     }
