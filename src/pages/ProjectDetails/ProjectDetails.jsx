c7571c06b95af22c8d3ad13fb76bd3ad44125ff0
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-20 20:21:51 -0400

    add handleDeleteProject helper function and conditional rendering for Edit & Delete functionality

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { projectDetails } from '../../services/projects-api'
+import * as projectAPI from '../../services/projects-api'
+import authService from '../../services/authService'
 
 class ProjectDetails extends Component {
     state = {
@@ -8,28 +9,48 @@ class ProjectDetails extends Component {
     }
 
     async componentDidMount() {
-        const project = await projectDetails(this.props.match.params.id)
+        const project = await projectAPI.projectDetails(this.props.match.params.id)
         this.setState({ project })
     }
+
+    handleDeleteProject = async id => {
+        if (authService.getUser()) {
+            await projectAPI.deleteProject(id)
+                .then(() => this.props.history.push('/projects'));
+        } else {
+            this.props.history.push('/login')
+        }
+    }
+
     render() {
         const project = this.state.project
+        const user = this.props.user
         return (
             <>
+
                 <div>
                     <h1>{project.name}</h1>
                     <p>Description: {project.description}</p>
                     <p>Hobby: {project.hobby}</p>
                     <p>Date Started: {project.dateStarted}</p>
                 </div>
-                <Link to="/projects">
-                    Projects
-                </Link><br />
-                <Link to="/editproject">
-                    Edit Project
-                </Link><br />
-                {/* <ProjectCard
-                    project={project}
-                /> */}
+
+
+                {user && (user._id === project.user) &&
+                    <div>
+                        <Link
+                            to={{
+                                pathname: '/editproject',
+                                state: { project }
+                            }}
+                        >
+                            Edit Project
+                            </Link><br></br>
+                        <button type="submit" onClick={() => this.handleDeleteProject(project._id)}>
+                            Delete Project
+                            </button><br></br>
+                    </div>
+                }
             </>
         );
     }
