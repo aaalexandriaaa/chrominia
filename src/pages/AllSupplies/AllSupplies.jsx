f788b082f1b23e6c0770a00417d333034c48c269
Jam Wheat <jamonwheat@gmail.com>
2020-09-21 14:34:58 -0400

    list supply types in supply cards

--- src/pages/AllSupplies/AllSupplies.jsx
+++ src/pages/AllSupplies/AllSupplies.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import SuppliesCard from '../../components/SuppliesCard/SuppliesCard'
 import * as supplyAPI from '../../services/supplies-api'
+import './AllSupplies.css'
 
 class AllSupplies extends Component {
   state = {
@@ -12,67 +14,15 @@ class AllSupplies extends Component {
     this.setState({ supplies })
   }
 
+  pullSupply = supply => {
+    const list = this.state.supplies.filter(each => each.type === supply)
+    return list
+  }
+
   render() { 
     return (
       <>
         <h1>All Supplies</h1>
-        <Link
-          to={{
-            pathname: '/viewbrushes'
-          }}
-        >
-          View All Brushes
-        </Link><br></br>
-        <Link
-          to={{
-            pathname: '/viewmaterials'
-          }}
-        >
-          View All Materials
-        </Link><br></br>
-        <Link
-          to={{
-            pathname: '/viewmodels'
-          }}
-        >
-          View All Models
-        </Link><br></br>
-        <Link
-          to={{
-            pathname: '/viewother'
-          }}
-        >
-          View Other
-        </Link><br></br>
-        <Link
-          to={{
-            pathname: '/viewpaintaccs'
-          }}
-        >
-          View All Paint Accessories
-        </Link><br></br>
-        <Link
-          to={{
-            pathname: '/viewpaints'
-          }}
-        >
-          View All Paints
-        </Link><br></br>
-        <Link
-          to={{
-            pathname: '/viewtools'
-          }}
-        >
-          View All Tools
-        </Link><br></br>
-        <Link
-          to={{
-            pathname: '/viewwishlist'
-          }}
-        >
-          View Wish List
-        </Link><br></br>
-        <br></br>
         <Link
           to={{
             pathname: '/addsupply'
@@ -80,6 +30,87 @@ class AllSupplies extends Component {
         >
           Add Supply
         </Link>
+        <Link
+          to={{
+            pathname: '/viewwishlist'
+          }}
+        >
+          View Wish List
+        </Link>
+        <div className="supply-card-container">
+          <Link
+            to={{
+              pathname: '/viewbrushes'
+            }}
+          >
+            <h3>Brushes</h3>
+            <SuppliesCard 
+              supplies = {this.pullSupply('brush')}
+            />  
+          </Link>
+          <Link
+            to={{
+              pathname: '/viewmaterials'
+            }}
+          >
+            <h3>Materials</h3>
+            <SuppliesCard 
+              supplies = {this.pullSupply('material')}
+            />  
+          </Link>
+          <Link
+            to={{
+              pathname: '/viewmodels'
+            }}
+          >
+            <h3>Models</h3>
+            <SuppliesCard 
+              supplies = {this.pullSupply('model')}
+            />  
+          </Link>
+          <Link
+            to={{
+              pathname: '/viewother'
+            }}
+          >
+            <h3>Other</h3>
+            <SuppliesCard 
+              supplies = {this.pullSupply('other')}
+            />  
+          </Link>
+          <Link
+            to={{
+              pathname: '/viewpaintaccs'
+            }}
+          >
+            <h3>Paint Accessories</h3>
+            <SuppliesCard 
+              supplies = {this.pullSupply('paintAcc')}
+            /> 
+          </Link>
+          <Link
+            to={{
+              pathname: '/viewpaints'
+            }}
+          >
+            <h3>View All Paints</h3>
+            <SuppliesCard 
+              supplies = {this.pullSupply('paint')}
+            /> 
+          </Link>
+          <Link
+            to={{
+              pathname: '/viewtools'
+            }}
+          >
+            <h3>Tools</h3>
+            <SuppliesCard 
+              supplies = {this.pullSupply('tool')}
+            /> 
+          </Link>
+        </div>
+        
+        
       </>
     );
   }
