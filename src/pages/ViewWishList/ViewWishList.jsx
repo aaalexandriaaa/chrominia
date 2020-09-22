9f27c4277a1d8f049471d0ad1bcee8305def6684
Jam Wheat <jamonwheat@gmail.com>
2020-09-22 12:00:03 -0400

    display wishlist in tabel

--- src/pages/ViewWishList/ViewWishList.jsx
+++ src/pages/ViewWishList/ViewWishList.jsx
@@ -22,7 +22,35 @@ class ViewWishList extends Component {
           }}
         >
           Add to Wish List
-        </Link>
+        </Link><br></br>
+        <table>
+         <thead>
+           <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Paint Type</th>
+            <th>Color</th>
+            <th>Size</th>
+            <th>Brand</th>
+            <th>Wish Lish?</th>
+            <th>Remove?</th>
+          </tr>
+         </thead>
+         <tbody>
+          {this.state.wishList.map((supply, idx) =>
+            <tr key={idx}>
+              <td>{supply.name}</td>
+              <td>{supply.type}</td>
+              <td>{supply.paintType}</td>
+              <td>{supply.color}</td>
+              <td>{supply.size}</td>
+              <td>{supply.brand}</td>
+              <td><button onClick={() => this.handleOwnSupply(supply)}>Wish List</button></td>
+              <td><button onClick={() => this.handleDeleteSupply(supply)}>Remove</button></td>
+            </tr>
+          )}
+          </tbody>
+        </table>
       </>
     );
   }
