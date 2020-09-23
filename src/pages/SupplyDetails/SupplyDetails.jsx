7b894217cdaa655d99b5e611946ab74b9df64d5c
cem aytac <cenko@MacBook-Air-3.local>
2020-09-23 13:09:41 -0400

    update table on supplydetails

--- src/pages/SupplyDetails/SupplyDetails.jsx
+++ src/pages/SupplyDetails/SupplyDetails.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import * as supplyAPI from '../../services/supplies-api'
+import './SupplyDetails.css'
+import Table from 'react-bootstrap/Table'
 
 const types = [
   ['paint', 'Paint'], ['brush', 'Brush'], ['model', 'Model'], ['paintacc', 'Paint Accessory'], ['other', 'Other Supply'], ['tool', 'Tool'], ['material', 'Material']
@@ -33,7 +35,7 @@ class SupplyDetails extends Component {
   printType = type => {
     let result
     types.forEach(each => {
-      if(each[0] === type){
+      if (each[0] === type) {
         result = each[1]
       }
     })
@@ -44,45 +46,45 @@ class SupplyDetails extends Component {
     const type = this.printType(this.props.match.params.supply)
     return (
       <>
-       <h1>{type} Details</h1>
-       <table>
-         <thead>
-           <tr>
-            <th>Name</th>
-            {this.props.match.params.supply === 'paint' &&
-              <>
-              <th>Paint Type</th>
-              <th>Color</th>
-              </>
-            }
-            <th>Size</th>
-            <th>Brand</th>
-            <th>Wish Lish?</th>
-            <th>Remove?</th>
-          </tr>
-         </thead>
-         <tbody>
-          {this.state.supplies.map((supply, idx) =>
-            <tr key={idx}>
-              <td>{supply.name}</td>
+        <h1>{type} Details</h1>
+        <Table striped bordered hover size='sm' variant='light'>
+          <thead>
+            <tr>
+              <th>Name</th>
               {this.props.match.params.supply === 'paint' &&
                 <>
-                <td>{supply.paintType}</td>
-                <td>{supply.color}</td>
+                  <th>Paint Type</th>
+                  <th>Color</th>
                 </>
               }
-              <td>{supply.size}</td>
-              <td>{supply.brand}</td>
-              <td><button onClick={() => this.handleWishListSupply(supply)}>Wish List</button></td>
-              <td><button onClick={() => this.handleDeleteSupply(supply)}>Remove</button></td>
-
+              <th>Size</th>
+              <th>Brand</th>
+              <th>Wish List?</th>
+              <th>Remove?</th>
             </tr>
-          )}
+          </thead>
+          <tbody>
+            {this.state.supplies.map((supply, idx) =>
+              <tr key={idx}>
+                <td>{supply.name}</td>
+                {this.props.match.params.supply === 'paint' &&
+                  <>
+                    <td>{supply.paintType}</td>
+                    <td>{supply.color}</td>
+                  </>
+                }
+                <td>{supply.size}</td>
+                <td>{supply.brand}</td>
+                <td><button onClick={() => this.handleWishListSupply(supply)} className='wishlistButton'>Wish List</button></td>
+                <td><button onClick={() => this.handleDeleteSupply(supply)} className='removeButton'>Remove</button></td>
+
+              </tr>
+            )}
           </tbody>
-        </table>
+        </Table>
       </>
     );
   }
 }
- 
+
 export default SupplyDetails;
