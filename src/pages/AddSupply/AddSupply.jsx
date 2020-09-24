5456a574cb629e0628fca51ec52bac62bc0ea4ab
cem aytac <cenko@MacBook-Air-3.local>
2020-09-24 15:35:57 -0400

    forms

--- src/pages/AddSupply/AddSupply.jsx
+++ src/pages/AddSupply/AddSupply.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import * as supplyAPI from '../../services/supplies-api'
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 class AddSupply extends Component {
   state = {
@@ -39,55 +40,59 @@ class AddSupply extends Component {
     return (
       <>
         <h1>Add Supply</h1>
-        <form ref={this.formRef} onSubmit={this.handleSubmit}>
-          <div>
-            <label >Name</label><br></br>
-            <input name="name" type="text" value={this.state.formData.name} onChange={this.handleChange} required />
-          </div><br></br>
-          <div>
-            <label >Type</label><br></br>
-            <select name="type" onChange={this.handleChange} value={this.state.formData.type} required>
-              <option value=""></option>
-              <option value="paint">Paint</option>
-              <option value="tool">Tool</option>
-              <option value="brush">Brush</option>
-              <option value="paintacc">Paint Accessory</option>
-              <option value="material">Material</option>
-              <option value="model">Model</option>
-              <option value="other">Other</option>
-            </select>
-          </div><br></br>
-          {(this.state.formData.type === "paint") ?
-            <>
-              <div>
-                <label >Type of Paint</label><br></br>
-                <input name="paintType" type="text" value={this.state.formData.paintType} onChange={this.handleChange} />
-              </div><br></br>
-              <div>
-                <label >Color</label><br></br>
-                <input name="color" type="text" value={this.state.formData.color} onChange={this.handleChange} />
-              </div><br></br>
+        <div className='form-div'>
+          <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+            <div>
+              <Form.Label style={{ padding: '5px' }}>Name</Form.Label>
+              <Form.Control name="name" type="text" value={this.state.formData.name} onChange={this.handleChange} required />
+            </div>
+            <div>
+              <Form.Label style={{ padding: '5px' }}>Type</Form.Label>
+              <Form.Control as="select" name="type" onChange={this.handleChange} value={this.state.formData.type} required>
+                <option value=""></option>
+                <option value="paint">Paint</option>
+                <option value="tool">Tool</option>
+                <option value="brush">Brush</option>
+                <option value="paintacc">Paint Accessory</option>
+                <option value="material">Material</option>
+                <option value="model">Model</option>
+                <option value="other">Other</option>
+              </Form.Control>
+            </div>
+            {(this.state.formData.type === "paint") ?
+              <>
+                <div>
+                  <Form.Label style={{ padding: '5px' }}>Type of Paint</Form.Label>
+                  <Form.Control name="paintType" type="text" value={this.state.formData.paintType} onChange={this.handleChange} />
+                </div>
+                <div>
+                  <Form.Label >Color</Form.Label>
+                  <Form.Control name="color" type="text" value={this.state.formData.color} onChange={this.handleChange} />
+                </div>
 
-            </>
-            : <> </>
-          }
-          <div>
-            <label >Size</label><br></br>
-            <input name="size" type="text" value={this.state.formData.size} onChange={this.handleChange} />
-          </div><br></br>
-          <div>
-            <label >Brand</label><br></br>
-            <input name="brand" type="text" value={this.state.formData.brand} onChange={this.handleChange} />
-          </div><br></br>
-          <Button
-            className='greenButton'
-            id='button'
-            type="submit"
-            disabled={this.state.invalidForm}
-          >
-            Add Supply
+              </>
+              : <> </>
+            }
+            <div>
+              <Form.Label style={{ padding: '5px' }}>Size</Form.Label>
+              <Form.Control name="size" type="text" value={this.state.formData.size} onChange={this.handleChange} />
+            </div>
+            <div>
+              <Form.Label style={{ padding: '5px' }}>Brand</Form.Label>
+              <Form.Control name="brand" type="text" value={this.state.formData.brand} onChange={this.handleChange} />
+            </div>
+            <div className='button-div'>
+              <Button
+                className="greenButton"
+                id="button"
+                type="submit"
+                disabled={this.state.invalidForm}
+              >
+                Add Supply
                     </Button>
-        </form>
+            </div>
+          </Form>
+        </div>
       </>
     );
   }
--- src/pages/AddWishList/AddWishList.jsx
+++ src/pages/AddWishList/AddWishList.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import * as supplyAPI from '../../services/supplies-api'
 import Button from 'react-bootstrap/Button'
 import './AddWishList.css'
+import Form from 'react-bootstrap/Form'
 
 class AddWishList extends Component {
   state = {
@@ -40,56 +41,58 @@ class AddWishList extends Component {
     return (
       <>
         <h1>Add to Wish List</h1>
-        <form ref={this.formRef} onSubmit={this.handleSubmit}>
-          <div>
-            <label >Name</label><br></br>
-            <input name="name" type="text" value={this.state.formData.name} onChange={this.handleChange} required />
-          </div><br></br>
-          <div>
-            <label >Type</label><br></br>
-            <select name="type" onChange={this.handleChange} value={this.state.formData.type} required>
-              <option value=""></option>
-              <option value="paint">Paint</option>
-              <option value="tool">Tool</option>
-              <option value="brush">Brush</option>
-              <option value="paintacc">Paint Accessory</option>
-              <option value="material">Material</option>
-              <option value="model">Model</option>
-              <option value="other">Other</option>
-            </select>
-          </div><br></br>
-          {this.state.formData.type === "paint" &&
-            <>
-              <div>
-                <label >Type of Paint</label><br></br>
-                <input name="paintType" type="text" value={this.state.formData.paintType} onChange={this.handleChange} />
-              </div><br></br>
-              <div>
-                <label >Color</label><br></br>
-                <input name="color" type="text" value={this.state.formData.color} onChange={this.handleChange} />
-              </div><br></br>
+        <div className='form-div'>
+          <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+            <div>
+              <Form.Label style={{ padding: '5px' }}>Name</Form.Label>
+              <Form.Control name="name" type="text" value={this.state.formData.name} onChange={this.handleChange} required />
+            </div>
+            <div>
+              <Form.Label >Type</Form.Label>
+              <Form.Control as="select" name="type" onChange={this.handleChange} value={this.state.formData.type} required>
+                <option value=""></option>
+                <option value="paint">Paint</option>
+                <option value="tool">Tool</option>
+                <option value="brush">Brush</option>
+                <option value="paintacc">Paint Accessory</option>
+                <option value="material">Material</option>
+                <option value="model">Model</option>
+                <option value="other">Other</option>
+              </Form.Control>
+            </div>
+            {this.state.formData.type === "paint" &&
+              <>
+                <div>
+                  <Form.Label style={{ padding: '5px' }}>Type of Paint</Form.Label>
+                  <Form.Control name="paintType" type="text" value={this.state.formData.paintType} onChange={this.handleChange} />
+                </div>
+                <div>
+                  <Form.Label >Color</Form.Label>
+                  <Form.Control name="color" type="text" value={this.state.formData.color} onChange={this.handleChange} />
+                </div>
 
-            </>
-          }
-          <div>
-            <label >Size</label><br></br>
-            <input name="size" type="text" value={this.state.formData.size} onChange={this.handleChange} />
-          </div><br></br>
-          <div>
-            <label >Brand</label><br></br>
-            <input name="brand" type="text" value={this.state.formData.brand} onChange={this.handleChange} />
-          </div>
-          <div className='button-div'>
-            <Button
-              className='greenButton'
-              id='button'
-              type="submit"
-              disabled={this.state.invalidForm}
-            >
-              Add to Wish List
+              </>
+            }
+            <div>
+              <Form.Label style={{ padding: '5px' }} >Size</Form.Label><br></br>
+              <Form.Control name="size" type="text" value={this.state.formData.size} onChange={this.handleChange} />
+            </div>
+            <div>
+              <Form.Label style={{ padding: '5px' }} >Brand</Form.Label><br></br>
+              <Form.Control name="brand" type="text" value={this.state.formData.brand} onChange={this.handleChange} />
+            </div>
+            <div className='button-div'>
+              <Button
+                className='greenButton'
+                id='button'
+                type="submit"
+                disabled={this.state.invalidForm}
+              >
+                Add to Wish List
             </Button>
-          </div>
-        </form>
+            </div>
+          </Form>
+        </div>
       </>
     );
   }
