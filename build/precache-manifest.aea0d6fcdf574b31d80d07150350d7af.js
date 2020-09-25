1db2399f5bebf3df7252b133f89b7e46ad2bb492
Jam Wheat <jamonwheat@gmail.com>
2020-09-25 12:09:00 -0400

    make some tweaks

---
+++ build/precache-manifest.aea0d6fcdf574b31d80d07150350d7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bd5d19de016b4b62eadaf6eed26a3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d6f433a24a8b1deb7b",
+    "url": "/static/css/main.4428b133.chunk.css"
+  },
+  {
+    "revision": "8b5b29fff15c25238239",
+    "url": "/static/js/2.ab5f9add.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ab5f9add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d6f433a24a8b1deb7b",
+    "url": "/static/js/main.1c48db0c.chunk.js"
+  },
+  {
+    "revision": "2d93ddce714052c0bbe7",
+    "url": "/static/js/runtime-main.e6b6e052.js"
+  }
+]);
